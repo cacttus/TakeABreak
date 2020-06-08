801c5ec47c5c0db385252d003bc2c48302c16dbc
Derek Page <america971@gmail.com>
2020-06-08 05:32:30 -0500

    Initial commit. Install MahApps.Metro via NuGet to run.

---
+++ ControlForm.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp1.UnManaged;
+using MahApps.Metro.Controls;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+//We need 1 timer per window for multi-window displays.
+namespace WpfApp1
+{
+   public enum TimerState
+   {
+      Run, Stop
+   }
+
+   /// <summary>
+   /// Interaction logic for ControlForm.xaml
+   /// </summary>
+   public partial class ControlForm : MetroWindow
+   {
+      List<TimerWindow> _timerWindows = new List<TimerWindow>();
+      WpfApp1.UnManaged.HotKey _hotkey = null;
+      double _curTimeMs = 0;
+      TimerState _timerState = TimerState.Stop;
+      Stopwatch _stopWatch = new Stopwatch();
+      MicroLibrary.MicroTimer dispatcherTimer = new MicroLibrary.MicroTimer();
+      Dispatcher _mainDisp = null;
+      bool _bFullscreen = true; 
+
+      public ControlForm()
+      {
+         InitializeComponent();
+         ShowTitleBar = false;
+         WindowStyle = WindowStyle.None;
+         WindowState = WindowState.Normal;
+         this.Visibility = Visibility.Hidden;
+         _mainDisp = System.Windows.Application.Current.MainWindow.Dispatcher;
+
+         //Create windows
+         foreach (var screen in Screen.AllScreens)
+         {
+            TimerWindow t = new TimerWindow();
+            t.Create(this, screen.Bounds);
+            _timerWindows.Add(t);
+         }
+         StartTimer();
+
+         SetupHotkeys();
+      }
+      public void Pause()
+      {
+         _stopWatch.Stop();
+         _curTimeMs = 0;
+         _timerState = TimerState.Stop;
+      }
+      public void StopAndHide()
+      {
+         Pause();
+         HideAllTimers();
+      }
+      private void SetupHotkeys()
+      {
+         _hotkey = new UnManaged.HotKey(Key.Space, KeyModifier.Ctrl, (x) =>
+         {
+            AddTime(30 * 1000);
+
+            foreach (TimerWindow t in _timerWindows)
+            {
+               t.PopStart(_bFullscreen);
+            }
+         });
+      }
+      private void AddTime(double time)
+      {
+         _curTimeMs += time;
+         _timerState = TimerState.Run;
+         UpdateWindowLabelTimers();
+      }
+      private void UpdateWindowLabelTimers()
+      {
+         foreach (TimerWindow t in _timerWindows)
+         {
+            t.UpdateLabelTimer(_curTimeMs);
+         }
+      }
+      private void Stop()
+      {
+         Pause();
+      }
+      private void HideAllTimers()
+      {
+         foreach (TimerWindow t in _timerWindows)
+         {
+            t.Hide();
+         }
+      }
+      private void StartTimer()
+      {
+         dispatcherTimer.MicroTimerElapsed += (x, y) =>
+         {
+            if (_timerState == TimerState.Run)
+            {
+               if (!_stopWatch.IsRunning)
+               {
+                  _stopWatch.Reset();
+                  _stopWatch.Start();
+               }
+
+               if (_stopWatch.ElapsedMilliseconds > 100)
+               {
+                  _stopWatch.Stop();
+
+                  if (_curTimeMs > 0)
+                  {
+                     _curTimeMs -= 100;
+                     UpdateWindowLabelTimers();
+                  }
+                  if (_curTimeMs <= 0)
+                  {
+                     _curTimeMs = 0;
+                     _mainDisp.BeginInvoke(new Action(() =>
+                     {
+                        StopAndHide();
+                     }));
+                  }
+                  else
+                  {
+                     _stopWatch.Reset();
+                     _stopWatch.Start();
+                  }
+               }
+            }
+
+         };
+         dispatcherTimer.Interval = (long)(10000);//millis
+         dispatcherTimer.Start();
+      }
+
+      private void _btnClose_Click(object sender, RoutedEventArgs e)
+      {
+         Environment.Exit(0);
+      }
+   }
+}
---
+++ TimerWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Drawing;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+using WpfApp1;
+using System.Windows.Threading;
+using System.Threading;
+using Hardcodet.Wpf;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace WpfApp1
+{
+
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class TimerWindow : MetroWindow
+   {
+
+      ControlForm _controlForm = null;
+      Rectangle _bounds = new Rectangle(0, 0, 0, 0);//Screen bounds.
+      Dispatcher _mainDisp = null;
+      double _origW = 0;
+      double _origH = 0;
+      #region Public: Methods
+      public TimerWindow()
+      {
+         InitializeComponent();
+         WindowStyle = WindowStyle.None;
+         _mainDisp = Application.Current.MainWindow.Dispatcher;
+         Visibility = Visibility.Hidden;
+         WindowStartupLocation = WindowStartupLocation.Manual;
+      }
+      public void Create(ControlForm cf, Rectangle bounds)
+      {
+         this._controlForm = cf;
+         this._bounds = bounds;
+         _origW = this.Width;
+         _origH = this.Height;
+         _lblTimer.Content = "0:00";
+      }
+      public void PopStart(bool fs)
+      {
+         Visibility = Visibility.Visible;
+         this.Topmost = true;
+
+         //Center
+         if (!fs)
+         {
+            this.Left = _bounds.Left + _bounds.Width / 2 - this.Width / 2;
+            this.Top = _bounds.Top + _bounds.Height / 2 - this.Height / 2;
+            this.Width = _origW;
+            this.Height = _origH;
+         }
+         else
+         {
+            this.Left = _bounds.Left;
+            this.Top = _bounds.Top;
+            this.Width = _bounds.Width;
+            this.Height = _bounds.Height;
+         }
+      }
+      #endregion
+
+      #region Private: UI Methods
+      private void _btnCancel_Click(object sender, RoutedEventArgs e)
+      {
+         _controlForm.StopAndHide();
+      }
+      private void _btnStop_Click(object sender, RoutedEventArgs e)
+      {
+         _controlForm.Pause();
+      }
+      #endregion
+
+      #region Private: Methods
+
+      public void UpdateLabelTimer(double time_ms)
+      {
+         _mainDisp.BeginInvoke(new Action(() =>
+         {
+            string ms = "." + (int)((time_ms % 1000) / 1000 * 10);
+            _lblTimer.Content = TimeSpan.FromMilliseconds(time_ms).ToString(@"mm\:ss") + ms;
+         }));
+      }
+
+      #endregion
+
+   }
+}
